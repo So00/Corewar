82d34491d236b2f0ccd786ba4c39d07a7718006f
Anthony TOURNERIE <atourner@e2r10p14.42.fr>
2019-03-30 08:54:26 +0100

    music

--- assembly/tamere.s
+++
@@ -1,8 +0,0 @@
-.name "
-OK OK OK
-KO KO KO"
-.comment "salut ca marche bien ou pas?"
-
-live %1
-live %1
-live %1
--- tamere.s
+++
@@ -1,7 +0,0 @@
-.name "
-OK OK OK
-KO KO KO"
-.comment "salut ca marche bien ou pas?"
-
-live:	live %1
-		live %1
